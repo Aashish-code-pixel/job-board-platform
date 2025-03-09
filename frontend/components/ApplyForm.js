53b1dc88b4cf9e2bd945f169b3fa1b0c2b7bee17
Aashish-code-pixel <144586455+Aashish-code-pixel@users.noreply.github.com>
2025-03-09 15:17:03 +0530

    Update ApplyForm.js

--- frontend/components/ApplyForm.js
+++ frontend/components/ApplyForm.js
@@ -1,1 +1,14 @@
+import React from 'react';
 
+const ApplyForm = () => {
+    return (
+        <form>
+            <input type='text' placeholder='Name' />
+            <input type='email' placeholder='Email' />
+            <textarea placeholder='Cover Letter'></textarea>
+            <button type='submit'>Apply</button>
+        </form>
+    );
+};
+
+export default ApplyForm;
