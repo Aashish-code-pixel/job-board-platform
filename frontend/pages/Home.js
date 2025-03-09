68d8267711637b35301b822b5826147015b8dab0
Aashish-code-pixel <144586455+Aashish-code-pixel@users.noreply.github.com>
2025-03-09 15:17:28 +0530

    Update Home.js

--- frontend/pages/Home.js
+++ frontend/pages/Home.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to Job Board</h1>
+            <Link to='/jobs'>View Jobs</Link>
+        </div>
+    );
+};
+
+export default Home;
