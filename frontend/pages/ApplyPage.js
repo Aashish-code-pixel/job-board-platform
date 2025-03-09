5a257a978a39e71e847cde874518a18c1cc7cc81
Aashish-code-pixel <144586455+Aashish-code-pixel@users.noreply.github.com>
2025-03-09 15:18:13 +0530

    Update ApplyPage.js

--- frontend/pages/ApplyPage.js
+++ frontend/pages/ApplyPage.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import ApplyForm from '../components/ApplyForm';
 
+const ApplyPage = () => {
+    return (
+        <div>
+            <h1>Apply for Job</h1>
+            <ApplyForm />
+        </div>
+    );
+};
+
+export default ApplyPage;
