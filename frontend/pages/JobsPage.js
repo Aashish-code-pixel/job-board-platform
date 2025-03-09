dbd27103f899ee041b6639450bca9864559825ec
Aashish-code-pixel <144586455+Aashish-code-pixel@users.noreply.github.com>
2025-03-09 15:17:53 +0530

    Update JobsPage.js

--- frontend/pages/JobsPage.js
+++ frontend/pages/JobsPage.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import JobList from '../components/JobList';
 
+const JobsPage = () => {
+    return (
+        <div>
+            <h1>Job Listings</h1>
+            <JobList />
+        </div>
+    );
+};
+
+export default JobsPage;
